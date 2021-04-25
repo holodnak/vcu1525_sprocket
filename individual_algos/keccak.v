a2cee32c8d87aec14952725a092187428c3377c9
James Holodnak <jamesholodnak@gmail.com>
2021-04-25 00:08:18 -0400

    Update keccak.v
    fix input FF

--- individual_algos/keccak.v
+++ individual_algos/keccak.v
@@ -150,7 +150,7 @@ module keccak_round (
 	
 	reg [63:0] r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,r13,r14,r15,r16,r17,r18,r19,r20,r21,r22,r23,r24;
 
-	always @ (posedge clk) begin
+	always @ (*) begin
 
 		i <= in;
 
